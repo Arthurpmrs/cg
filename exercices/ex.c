a9c853031ff422f2e2282c9c5c3321c49573af71
Arthur Soares <arthurpmrs@gmail.com>
2025-04-09 19:41:30 -0300

    exercicio de sala

--- exercices/ex.c
+++ exercices/ex.c
@@ -21,7 +21,22 @@ void init(void){
 // Função callback chamada para fazer o desenho
 void display(void){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	    
+    
+    // glPushMatrix();
+    //     glColor3f(0.0, 0.0, 1.0);
+
+    //     glTranslatef(2.0, 3.0, 0.0);
+    //     glRotatef((GLfloat) year, 0.0, 0.0, 1.0);
+    //     glTranslatef(-2.0, -3.0, 0.0);
+
+    //     glBegin(GL_TRIANGLES);
+    //         glVertex3i(2, 3, 0);
+    //         glVertex3i(4, 3, 0);
+    //         glVertex3i(3, 4.7, 0);
+    //     glEnd();
+    // glPopMatrix();
+
+
 	glPushMatrix();
         glColor3f(0.0, 0.0, 1.0);
         glutWireSphere(1.0, 20, 16);
@@ -30,8 +45,8 @@ void display(void){
     glPushMatrix();
         glColor3f(1.0, 0.7, 1.0);
 
-        // glRotatef((GLfloat) year, 1.0, 0.0, 0.0);
-        glTranslatef(0.0, 2.0, 0.0);
+        glRotatef((GLfloat) year, 0.0, 1.0, 0.0);
+        glTranslatef(2.0, 0.0, 0.0);
 
         glutWireSphere(0.4, 20 ,16);
     glPopMatrix();
@@ -39,11 +54,11 @@ void display(void){
     glPushMatrix();
         glColor3f(1.0, 1.0, 1.0);
 
-        // glRotatef((GLfloat) year, 1.0, 0.0, 0.0);
-        glTranslatef(0.0, 2.0, 0.0);
+        // glRotatef((GLfloat) year, 0.0, 1.0, 0.0);
+        // glTranslatef(2.0, 0.0, 0.0);
 
-        glRotatef((GLfloat) year, 0.0, 1.0, 0.0);
-        glTranslatef(0.8, 0.0, 0.0);
+        glRotatef((GLfloat) year, 1.0, 0.0, 0.0);
+        glTranslatef(0.0, 0.0, 0.8);
 
         glutWireSphere(0.2, 20, 16);
     glPopMatrix();
@@ -94,13 +109,14 @@ void display(void){
 }
 
 void reshape (int w, int h){
+	// glViewport(0, 0, (GLsizei) w, (GLsizei) h);
 	glViewport(0, 0, (GLsizei) w, (GLsizei) h);
     
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
     
 	gluPerspective(60.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
-	gluLookAt(0.0, 0.0, 7, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); //posicao da camera
+	gluLookAt(0.0, 0.0, 15.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); //posicao da camera
     
 }
 
